45e9c7d59536f509b469d5e91a6ad0bb2d4bd2b5
Kevin Christanto <kevinchrist75@gmail.com>
2023-07-19 16:40:28 +0700

    Initial Commit

---
+++ explore-page/explore_pageApp.swift
@@ -0,0 +1,17 @@
+//
+//  explore_pageApp.swift
+//  explore-page
+//
+//  Created by Kevin Christanto on 19/07/23.
+//
+
+import SwiftUI
+
+@main
+struct explore_pageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
